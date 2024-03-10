356c122c84d8bf7dd908bc7f2bb33cfb890c8683
ShokoladniyGlaz <voronario@inbox.ru>
2024-03-10 17:16:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,49 @@
 // Получаем элементы слайдера
 const prevButton = document.querySelector('.prev-button');
 const nextButton = document.querySelector('.next-button');
-const slider = document.querySelector('.slider');
-const slides = Array.from(slider.querySelectorAll('.slide'));
-const slider_indicators = document.querySelector('.slider_indicators');
-const indicators = Array.from(slider_indicators.querySelector('.indicator'));
-const slideCount = slides.length;
-const indicatorCount = indicators.length;
-let slideIndex = 0;
+const wrapper = document.querySelector('.slider-container')
+const slidesField = document.querySelector('.slider');
+const slides = slidesField.querySelectorAll(".slide");
+const dotParent = document.querySelector('.slider_indicators');
+const dots = dotParent.querySelectorAll(".indicator");
+const width = window.getComputedStyle(wrapper).width;
+let slideIndex = 1;
+let offset = 0
 
-// Устанавливаем обработчики событий для кнопок
-prevButton.addEventListener('click', showPreviousSlide);
-nextButton.addEventListener('click', showNextSlide);
+slidesField.style.width = slides.length * 100 + '%';
+slidesField.style.display = 'flex'
+slidesField.style.transition = 'all .5s'
 
-// Функция для показа предыдущего слайда
-function showPreviousSlide() {
-    slideIndex = (slideIndex - 1 + slideCount) % slideCount;
-    updateSlider();
-    thisSlide();
-}
-
-// Функция для показа следующего слайда
-function showNextSlide() {
-    slideIndex = (slideIndex + 1) % slideCount;
-    updateSlider();
-    thisSlide();
-}
-
-// Функция для обновления отображения слайдера
-function updateSlider() {
-    slides.forEach((slide, index) => {
-        if (index === slideIndex) {
-            slide.style.display = 'block';
-        } else {
-            slide.style.display = 'none';
-        }
+dots.forEach((item) => {
+    item.style.opacity = "0.5";
+});
+dots[slideIndex - 1].style.opacity = "1";
+prevButton.addEventListener('click', () => {
+    if (offset !== 0) {
+        offset -= +width.slice(0, (width.length - 2));
+        slideIndex--;
+    } else {
+        offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+        slideIndex = slides.length;
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`
+    dots.forEach((item) => {
+        item.style.opacity = "0.5";
     });
-}
+    dots[slideIndex - 1].style.opacity = "1";
+})
+nextButton.addEventListener('click', () => {
+    if (offset === +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        offset = 0
+        slideIndex = 1;
+    } else {
+        offset += +width.slice(0, width.length - 2);
+        slideIndex++;
+    }
+    slidesField.style.transform = `translateX(-${offset}px)`
 
-//Индикаторы
-function thisSlide() {
-    indicators.forEach((indicator, index) => {
-        if (index !== slideIndex) {
-            indicator.style.opacity = "0.5";
-        } else {
-            indicator.style.opacity = "1";
-        }
+    dots.forEach(item => {
+        item.style.opacity = '0.5'
     });
-}
-// Инициализация слайдера
-updateSlider();
-thisSlide();
+    dots[slideIndex - 1].style.opacity = '1'
+})
